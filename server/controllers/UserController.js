87058abcea53263bb9e4fa90d80f77d46189f2b0
SOURYA DEY <souryadey031@gmail.com>
2025-08-03 17:30:46 +0530

    feat: create UserController for user data retrieval

---
+++ server/controllers/UserController.js
@@ -0,0 +1,15 @@
+// Get/api/user
+
+export const getUserData = async (req, res) => {
+  try {
+    const role = req.user.role;
+    const recentSearchCities = req.user.recentSearchCities;
+    res.json({
+      success: true,
+      role,
+      recentSearchCities,
+    });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
