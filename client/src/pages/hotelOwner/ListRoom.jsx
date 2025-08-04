d97b31266a041569ad8503e63fcbff46016ffeb2
SOURYA DEY <souryadey031@gmail.com>
2025-08-04 20:57:17 +0530

    feat: include user and currency in ListRoom component for enhanced room display

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -8,7 +8,7 @@ import { useAppContext } from "../../context/appContext";
 
 const ListRoom = () => {
   const [rooms, setRooms] = useState([]);
-  const { axios, getToken } = useAppContext();
+  const { axios, getToken, user, currency } = useAppContext();
 
   // Fetch rooms from the server
   const fetchRooms = async () => {
@@ -85,7 +85,7 @@ const ListRoom = () => {
                   {item.amenities.join(", ")}
                 </td>
                 <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
-                  {item.pricePerNight}
+                  {currency} {item.pricePerNight}
                 </td>
                 <td className="py-3 px-4 border-t border-gray-300 text-sm text-red-500 text-center">
                   <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
