a6ef3745ebbca86c0053272c9483654b1bcdcee3
SOURYA DEY <souryadey031@gmail.com>
2025-08-05 17:01:10 +0530

    feat: add RecommendedHotel component to display filtered hotel recommendations

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import FeaturedDestination from '../components/FeaturedDestination'
-import ExclusiveOffers from '../components/ExclusiveOffers'
-import Testimonial from '../components/Testimonial'
-import NewsLetter from '../components/NewsLetter'
+import React from "react";
+import Hero from "../components/Hero";
+import FeaturedDestination from "../components/FeaturedDestination";
+import ExclusiveOffers from "../components/ExclusiveOffers";
+import Testimonial from "../components/Testimonial";
+import NewsLetter from "../components/NewsLetter";
+import RecommendedHotel from "../components/RecommendedHotel";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <RecommendedHotel />
       <FeaturedDestination />
       <ExclusiveOffers />
       <Testimonial />
       <NewsLetter />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
